9f39ef0e83c8f0776dac44ac50e5c22ed0e1b9b0
qharny <kabuteymanasseh5@gmail.com>
2025-03-31 02:24:17 +0000

    first commit

---
+++ bin/archive_extractor.dart
@@ -0,0 +1,5 @@
+import 'package:archive_extractor/archive_extractor.dart' as archive_extractor;
+
+void main(List<String> arguments) {
+  print('Hello world: ${archive_extractor.calculate()}!');
+}
---
+++ test/archive_extractor_test.dart
@@ -0,0 +1,8 @@
+import 'package:archive_extractor/archive_extractor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
